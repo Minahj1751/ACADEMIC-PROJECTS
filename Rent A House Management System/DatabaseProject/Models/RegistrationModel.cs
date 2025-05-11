223ed40e241e422d23df7bb6662482bc165fc195
MD MINHAJUL <minhajtanvir1@gmail.com>
2025-05-11 22:48:06 +0600

    Dot net MVC Project

---
+++ Rent A House Management System/DatabaseProject/Models/RegistrationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseProject.Models
+{
+    public class RegistrationModel
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Password { get; set; }
+    }
+}
