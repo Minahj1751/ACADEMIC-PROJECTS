edbc66a257e96b98d57c1fb4e21a4f63fd12feee
MD Minhajul Islam <minhajtanvir1@gmail.com>
2025-05-11 22:55:27 +0600

    Delete Java directory

--- Java/Admission System/interfaces/StudentOparation.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-import java .io.*;
-public interface StudentOpatation
-{
-	void insertStudent(Student s);
-	void removeStudent(Student s);
-	Customer getStudent(String regi);
-	void showAllStudent();
-}
